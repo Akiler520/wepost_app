fa5877451e4380291abb46df8bde06b07ff1ff05
martin <yumin@ppctech.com.cn>
2018-07-08 13:17:02 +0800

    update v1

--- js/mt.message.js
+++ js/mt.message.js
@@ -45,6 +45,59 @@ function sendLocalMsg(message){
     plus.push.createMessage("大牛消息:" + message, "LocalMSG", {cover:false});
 }
 
+
+
+// new message
+//mui.plusReady(function() {
+//  //仅支持竖屏显示
+//  plus.screen.lockOrientation("portrait-primary");
+//
+//  document.addEventListener("newintent", function() {
+//      openWebviewFromOthers();
+//  });
+//
+//  plus.push.addEventListener("click", function(msg) {
+//      console.log("push click");
+//      pushGetRun(msg.payload);
+//  }, false);
+//  plus.push.addEventListener("receive", function(msg) {
+//      //获取透传数据
+//      var data = JSON.parse(msg.payload);
+//      //创建本地消息
+//        plus.push.createMessage(data.content, data.payload, {
+//            title: data.title,
+//            cover: false
+//        });
+//  }, false);
+//  
+//  openWebviewFromOthers();
+//});
+//function pushGetRun(payload) {
+//  payload = JSON.parse(payload);
+//  var id = payload.id;
+//  var autoShow = payload.autoshow;
+//  var event = payload.event;
+//  var params = JSON.stringify(payload.params);
+//  //......//用参数打开指定页面
+//}
+////获取通知栏（app未启动）点击、第三方程序启动本app
+//function openWebviewFromOthers() {
+//  var args = plus.runtime.arguments;
+//      if(args) {
+//          pushGetRun(args);
+//      }
+//}
+
+
+
+
+
+
+
+
+
+
+// old 
 mui.plusReady(function(){
 	var messageEventNew = setInterval("mtGetPushMessage()", 10000);
 	
@@ -60,9 +113,19 @@ mui.plusReady(function(){
 		
 		plus.push.clear();
 	}, false );
-  
+	
+	plus.push.addEventListener("receive", function(msg) {
+        //获取透传数据
+        var data = JSON.parse(msg.payload);
+        //创建本地消息
+        
+        sendLocalMsg(data.content);
+    }, false);
+
 	window.addEventListener("refresh", function (e) {
-		location.reload();
+		if(!e.detail){
+			location.reload();
+		}
 		
 		console.log("refresh detail: "+e.detail);
 	});
---
+++ libs/verify/readme.txt
@@ -0,0 +1,8 @@
+密码：所有证书默认密码为q1234
+APPIDs: com.chengddn.app
+
+其他描述：
+distribution.p12               ――为发布开发者证书
+push.p12                       ――为推送证书（没有推送功能，请忽略）
+distribution.mobileprovision   ――为配置文件
+appBymePush.pem                ――为推送PEM格式推送证书（推送没有用到pem请忽略）
